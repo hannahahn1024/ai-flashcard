3fbcbd617c539f452a88249878562e11c76ecea0
Justin Lee <roof558@gmail.com>
2024-08-17 18:29:58 -0700

    1:38:23

--- app/page.js
+++ app/page.js
@@ -1,12 +1,44 @@
-import Image from "next/image"
-import getStripe from "@/utils/get-stripe"
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
-import { AppBar, Container, Toolbar, Typography, Button, Box, Grid } from "@mui/material"
-import Head from "next/head"
+import Image from "next/image";
+import getStripe from "@/utils/get-stripe";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+  Grid,
+} from "@mui/material";
+import Head from "next/head";
 
 export default function Home() {
+  const handleSubmit = async () => {
+    const checkoutSession = await fetch("/api/checkout_session", {
+      method: "POST",
+      headers: {
+        origin: "http://localhost:3000",
+      },
+    });
+
+    const checkoutSessionJson = await checkoutSession.json();
+
+    if (checkoutSession.statusCode === 500) {
+      console.error(checkoutSession.message);
+      return;
+    }
+
+    const stripe = await getStripe();
+    const { error } = await stripe.redirectToCheckout({
+      sessionId: checkoutSessionJson.id,
+    });
+
+    if (error) {
+      console.warn(error.message);
+    }
+  };
   return (
-    <Container maxWidth = "100vw">
+    <Container maxWidth="100vw">
       <Head>
         <title>Flashcard SaaS</title>
         <meta name="description" content="Create flashcard from your text" />
@@ -14,10 +46,12 @@ export default function Home() {
 
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" style={{flexGrow: 1}}>Flashcard SaaS</Typography>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Flashcard SaaS
+          </Typography>
           <SignedOut>
             <Button color="inherit" href="/sign-in">
-              {' '}
+              {" "}
               Login
             </Button>
             <Button color="inherit" href="/sign-up">
@@ -25,32 +59,30 @@ export default function Home() {
             </Button>
           </SignedOut>
           <SignedIn>
-            <UserButton/>
+            <UserButton />
           </SignedIn>
         </Toolbar>
       </AppBar>
-      
-      <Box 
+
+      <Box
         sx={{
-          textAlign: 'center',
+          textAlign: "center",
           my: 4,
         }}
       >
-        <Typography variant="h2" gutterBottom>Welcome to Flashcard SaaS</Typography>
+        <Typography variant="h2" gutterBottom>
+          Welcome to Flashcard SaaS
+        </Typography>
         <Typography variant="h5" gutterBottom>
-          {' '}
+          {" "}
           The easiest way to make flashcards from your text
         </Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          sx={{mt: 2}}
-        >
+        <Button variant="contained" color="primary" sx={{ mt: 2 }}>
           GET STARTED
         </Button>
       </Box>
-      <Box sx={{my:6}}>
-        <Typography variant="h4" components='h2' gutterBottom>
+      <Box sx={{ my: 6 }}>
+        <Typography variant="h4" components="h2" gutterBottom>
           Features
         </Typography>
         <Grid container spacing={4}>
@@ -59,8 +91,9 @@ export default function Home() {
               Easy Text Input
             </Typography>
             <Typography>
-              {' '}
-              Simply input your text and let our software do the rest. Creating flashcards has never been easier
+              {" "}
+              Simply input your text and let our software do the rest. Creating
+              flashcards has never been easier
             </Typography>
           </Grid>
           <Grid item xs={12} md={4}>
@@ -68,8 +101,9 @@ export default function Home() {
               Smart Flashcards
             </Typography>
             <Typography>
-              {' '}
-              Out AI intelligently breaks down your text into concise flashcards, perfect for studying
+              {" "}
+              Out AI intelligently breaks down your text into concise
+              flashcards, perfect for studying
             </Typography>
           </Grid>
           <Grid item xs={12} md={4}>
@@ -77,21 +111,24 @@ export default function Home() {
               Accessible Anywhere
             </Typography>
             <Typography>
-              {' '}
-              Access your flashcards from any device, at any time. Study on the go with ease.
+              {" "}
+              Access your flashcards from any device, at any time. Study on the
+              go with ease.
             </Typography>
           </Grid>
         </Grid>
       </Box>
-      <Box sx={{my:6, textAlign: 'center'}}>
-        <Typography variant="h4" gutterBottom>Pricing</Typography>
+      <Box sx={{ my: 6, textAlign: "center" }}>
+        <Typography variant="h4" gutterBottom>
+          Pricing
+        </Typography>
         <Grid container spacing={4}>
           <Grid item xs={12} md={6}>
-            <Box 
+            <Box
               sx={{
                 p: 3,
-                border: '1px solid',
-                borderColor: 'grey.300',
+                border: "1px solid",
+                borderColor: "grey.300",
                 borderRadius: 2,
               }}
             >
@@ -102,20 +139,20 @@ export default function Home() {
                 $5 / month
               </Typography>
               <Typography>
-                {' '}
+                {" "}
                 Access to basic flashcard features and limited storage.
               </Typography>
-              <Button variant="contained" color="primary" sx={{mt: 2}}>
+              <Button variant="contained" color="primary" sx={{ mt: 2 }}>
                 Choose basic
               </Button>
             </Box>
           </Grid>
           <Grid item xs={12} md={6}>
-          <Box 
+            <Box
               sx={{
                 p: 3,
-                border: '1px solid',
-                borderColor: 'grey.300',
+                border: "1px solid",
+                borderColor: "grey.300",
                 borderRadius: 2,
               }}
             >
@@ -126,10 +163,15 @@ export default function Home() {
                 $10 / month
               </Typography>
               <Typography>
-                {' '}
+                {" "}
                 Unlimited flashcards and storage, with priority support.
               </Typography>
-              <Button variant="contained" color="primary" sx={{mt: 2}}>
+              <Button
+                variant="contained"
+                color="primary"
+                sx={{ mt: 2 }}
+                onClick={handleSubmit}
+              >
                 Choose Pro
               </Button>
             </Box>
@@ -137,5 +179,5 @@ export default function Home() {
         </Grid>
       </Box>
     </Container>
-  )
+  );
 }
